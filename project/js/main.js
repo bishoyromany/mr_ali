01c452ac0207544d3d0b3889185aa2d32e76d786
Bishoy Romany <7bishoyromany7@gmail.com>
2019-11-07 02:58:33 +0200

    finished homepage for computer design

--- project/js/main.js
+++ project/js/main.js
@@ -4,7 +4,33 @@ $(document).ready(function(){
      */
     new WOW().init();
 
+    /**
+     * scroll to top
+     */
+    $("a[href='#top']").click(function() {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
 
+
+    /**
+     * add fixed top to navbar when banner is scrolled
+     */
+    $(document).on('scroll', function() {
+        if($(document).scrollTop() >= $('#BannerMain').innerHeight()) {
+            $("#navbar").addClass("fixed-top");
+        }else{
+            $("#navbar").removeClass("fixed-top");
+        }
+    })
+
+
+    // $(".navSubContainer").hover(function(){
+    //     $(this).css('overflow' , '');
+    // });
+    // $(".navSubContainer").mouseleave(function(){
+    //     $(this).css('overflow' , 'hidden');
+    // });
     /**
      * show video
      */
@@ -25,4 +51,27 @@ $(document).ready(function(){
         }, 300);
         $('iframe').attr('src', $('iframe').attr('src'));
     });
+
+    // animated scrolling to id
+    function scrollToId(id , delay){
+        $("html, body").animate({ scrollTop: $('#'+id).offset().top }, delay);
+    }
+
+    $('.hasSub ul li a').click(function(){
+        console.log("clicked");
+        var ids = $(this).attr('href').split('#');
+        if(ids.length > 0){
+            scrollToId(ids[1] , 1000); 
+            window.location.href = $(this).attr('href');
+            return false;
+        }
+
+    });
+
+    var scrollIds = window.location.href.split('#');
+    if(scrollIds.length > 0){
+        setTimeout(function(){
+            scrollToId(scrollIds[1] , 500);
+        } , 1000);
+    }
 });
